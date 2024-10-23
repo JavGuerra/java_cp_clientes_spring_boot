a9f11d0d091f60b0aea318be8b2767682ba8cd88
JavGuerra <javguerra@badared.com>
2024-10-23 08:43:20 +0200

    JavaDoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.javguerra"},{"l":"com.javguerra.controllers"},{"l":"com.javguerra.entities"},{"l":"com.javguerra.repositories"},{"l":"com.javguerra.services"},{"l":"com.javguerra.utils"}];updateSearchResults();
